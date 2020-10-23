bb288275769c365514913be62c7a876d73ac7579
Gokul Kumar Gopal <gokulkumar.gopal>
2020-10-23 23:08:24 +0530

    Bug fixed : redirect to users/registration errored out when password does not match the strength. Removed the forward and introduced redirect with a flash attribute

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -41,13 +41,13 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user, Model model) {
+    public String registerUser(User user, RedirectAttributes redirectAttributes) {
         if(userService.checkPasswordStrength(user.getPassword())) {
             userService.registerUser(user);
             return "redirect:/users/login";
         } else {
-            model.addAttribute("passwordTypeError", "Password must contain atleast 1 alphabet, 1 number & 1 special character");
-            return "forward:/users/registration";
+            redirectAttributes.addFlashAttribute("passwordTypeError", "Password must contain atleast 1 alphabet, 1 number & 1 special character");
+            return "redirect:/users/registration";
         }
     }
 
